f62591976604fabca4dfdcf8b76d79d8d810dc25
ricolaws <eatfrenzy@gmail.com>
2021-06-27 19:17:53 -0700

    added a text scroll animation

--- scripts.js
+++ scripts.js
@@ -8,13 +8,17 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 let totalCardsFlipped = 0;
+let msgIndex = 0;
+let text = 'pls, choose a card...';
+let speed = 50;
 
 shuffle();
 
+// this function will be called on a card when clicked
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-
+  // the flip class flips the card over
   this.classList.toggle('flip');
 
   if (!hasFlippedCard) {
@@ -28,9 +32,9 @@ function flipCard() {
   }
 }
 
+// compare the datasets of the chosen cards to determine if they are a match
 function checkForMatch() {
   let isMatch = firstCard.dataset.card === secondCard.dataset.card;
-
   isMatch ? disableCards() : unFlipCards();
 }
 
@@ -38,35 +42,58 @@ function randomNumber() {
   return Math.floor(Math.random() * 3 + 1);
 }
 
+// select a random message to display when the cards don't match
 function failMsg() {
   let failPick = randomNumber();
+  catMsg.innerHTML = '';
+  msgIndex = 0;
+  // msgIndex = 0;
   if (failPick === 1) {
-    catMsg.textContent = 'Hmmmm... nice try.';
+    text = 'Hmmmm... nice try.';
+    typingEffect();
   } else if (failPick === 2) {
-    catMsg.textContent = 'Those two look different';
+    text = 'Those two look different.';
+    typingEffect();
   } else {
-    catMsg.textContent = 'Uh oh.';
+    text = 'Uh oh. Not those.';
+    typingEffect();
   }
 }
 
+// select a random message to display when the cards match
 function matchMsg() {
   let matchPick = randomNumber();
+  catMsg.innerHTML = '';
+  msgIndex = 0;
   if (matchPick === 1) {
-    catMsg.textContent = `It's a match!`;
+    text = `It's a match!`;
+    typingEffect();
   } else if (matchPick === 2) {
-    catMsg.textContent = 'Not bad.';
+    text = 'You are get it.';
+    typingEffect();
   } else {
-    catMsg.textContent = 'Nice job!';
+    text = 'Good job!';
+    typingEffect();
   }
 }
 
+function typingEffect() {
+  document.documentElement.style.setProperty('--steps', text.length);
+  if (msgIndex < text.length) {
+    catMsg.innerHTML += text.charAt(msgIndex);
+    msgIndex++;
+    setTimeout(typingEffect, speed);
+  }
+}
+
+// remove event listeners from cards and mark them as outOfPlay
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
   firstCard.classList.add('outOfPlay');
   secondCard.classList.add('outOfPlay');
   matchMsg();
-  gameOver();
+  isGameOver();
   resetBoard();
 }
 
@@ -76,7 +103,7 @@ function unFlipCards() {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
     resetBoard();
-  }, 1000);
+  }, 2000);
   failMsg();
 }
 
@@ -92,18 +119,19 @@ function shuffle() {
   });
 }
 
-function gameOver() {
+function isGameOver() {
   totalCardsFlipped = document.querySelectorAll('.outOfPlay').length;
   if (totalCardsFlipped === 12) {
-    catMsg.textContent = `You found them all! Amazing!`;
+    text = `You found them all! Amazing!`;
+    typingEffect();
     setTimeout(() => {
       cards.forEach(card => card.classList.toggle('flip'));
       totalCardsFlipped = 0;
       resetBoard();
-    }, 2000);
+    }, 3000);
   }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-console.log(randomNumber(), randomNumber());
+typingEffect();
